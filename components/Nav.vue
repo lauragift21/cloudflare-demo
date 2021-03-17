c5be29179f382534dc4445760de7e0ab67afff91
Gift Egwuenu <gift.egwuenu@valtech.com>
2021-03-17 19:54:51 +0100

    add youtube link

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,7 +2,7 @@
   <div class="mt-4 text-xl">
     <ul class="flex justify-end">
       <li class="mr-6">
-        <a class="text-black-500 hover:text-orange-500" href="">Tutorial</a>
+        <a class="text-black-500 hover:text-orange-500" href="https://youtu.be/Fkm3e0j2sbU">Tutorial</a>
       </li>
       <li class="mr-6">
         <a
